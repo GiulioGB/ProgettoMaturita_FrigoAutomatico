110e559cea049dddeb809c6a51a11f1ed9060fd7
Ballabio Giulio <giuliogb1997@gmail.com>
2016-04-18 18:05:15 +0200

    Aggiunto Form C# con connessione a DataBase

---
+++ Implementazione/Software/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Inserisci = new System.Windows.Forms.Button();
+            this.NomeProdotto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.scadenza = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Inserisci
+            // 
+            this.Inserisci.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Inserisci.Location = new System.Drawing.Point(150, 264);
+            this.Inserisci.Name = "Inserisci";
+            this.Inserisci.Size = new System.Drawing.Size(346, 42);
+            this.Inserisci.TabIndex = 0;
+            this.Inserisci.Text = "Inserisci Prodotto";
+            this.Inserisci.UseVisualStyleBackColor = true;
+            this.Inserisci.Click += new System.EventHandler(this.Inserisci_Click);
+            // 
+            // NomeProdotto
+            // 
+            this.NomeProdotto.Location = new System.Drawing.Point(269, 44);
+            this.NomeProdotto.Name = "NomeProdotto";
+            this.NomeProdotto.Size = new System.Drawing.Size(227, 20);
+            this.NomeProdotto.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(147, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Data di scadenza";
+            // 
+            // scadenza
+            // 
+            this.scadenza.Location = new System.Drawing.Point(269, 87);
+            this.scadenza.Name = "scadenza";
+            this.scadenza.Size = new System.Drawing.Size(227, 20);
+            this.scadenza.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 328);
+            this.Controls.Add(this.scadenza);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NomeProdotto);
+            this.Controls.Add(this.Inserisci);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Inserisci;
+        private System.Windows.Forms.TextBox NomeProdotto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox scadenza;
+    }
+}
+
