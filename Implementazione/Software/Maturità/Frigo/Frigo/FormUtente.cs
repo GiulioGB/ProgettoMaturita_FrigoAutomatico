0ee6052dc31a387303e9dcf245bce4fc008776f9
Ballabio Giulio <giuliogb1997@gmail.com>
2016-05-03 16:36:50 +0200

    Aggiungi Elimina prodotto , Login Reg. utente , QR sito , revisione grafica

---
+++ Implementazione/Software/Maturità/Frigo/Frigo/FormUtente.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Frigo
+{
+    public partial class FormUtente : UserControl
+    {
+        public FormUtente()
+        {
+            InitializeComponent();
+        }
+
+        private void CONFERMA_Click(object sender, EventArgs e)
+        {
+            ConnessioneDB conn = new ConnessioneDB();
+            
+            //controllo che siano stati compilati i campi
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "" || textBox5.Text == "" || textBox6.Text == "")
+            {
+                MessageBox.Show("Compila tutti i campi prima di confermare ! ");
+            }
+            else
+            {
+                //salva l'utente
+                conn.Salva(textBox1, textBox2, textBox3, textBox4, textBox5, textBox6);
+                
+                //pulisco il "form"
+                Reset();
+                
+                //torno indietro
+                AggiungiFamiliare agg = new AggiungiFamiliare();
+                Controls.Add(agg);
+                agg.BringToFront();
+                this.Refresh();
+
+            }
+            
+        }
+
+        public void Reset()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+        }
+
+        private void Menu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Aggiungi_Click(object sender, EventArgs e)
+        {
+            AggiungiFamiliare agg = new AggiungiFamiliare();
+            Controls.Add(agg);
+            agg.BringToFront();
+            this.Refresh();
+        }
+
+        
+        
+    }
+}
---
+++ Implementazione/Software/Maturità/Frigo/Frigo/WebCam.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+//using AForge.Video.FFMPEG;
+using System.Drawing.Drawing2D;
+//using AForge.Video.VFW;
+using System.IO;
+//using System.Windows.Media.Imaging;
+using System.Drawing.Imaging;
+//using System.Windows.Threading;
+using System.Threading;
+//MIE
+using WebCam_Capture;
+using CsCAMLibrary;
+using MySql.Data.MySqlClient;
+
+
+namespace Frigo
+{
+    public partial class WebCam : UserControl
+    {
+        private FilterInfoCollection VideoCaptureDevices;
+        private VideoCaptureDevice FinalVideo=null;
+        //public VideoFileWriter writer = new VideoFileWriter();
+
+        ConnessioneDB conn = new ConnessioneDB();
+
+        public WebCam()
+        {
+            InitializeComponent();
+
+            VideoCaptureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo VideoCaptureDevice in VideoCaptureDevices)
+            {
+                comboBox1.Items.Add(VideoCaptureDevice.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+
+            conn.ApriConnessione();
+
+            
+        }  
+
+        private void WebCam_Load(object sender, EventArgs e)
+        {
+            //clicco in modo automatico il bottone
+            start.PerformClick();
+            //avvio il timer
+            timer1.Interval = 1500;
+            timer1.Start();
+            
+        } 
+        void FinalVideo_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            try
+            {
+               Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone(); 
+                cam.Image = bitmap;
+            }
+            catch
+            {
+
+
+            }
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            //writer.Open(@"d:\\newVid.avi", 640, 480, 25, VideoCodec.MPEG4);
+
+            FinalVideo = new VideoCaptureDevice(VideoCaptureDevices[comboBox1.SelectedIndex].MonikerString);
+            FinalVideo.NewFrame += new NewFrameEventHandler(FinalVideo_NewFrame);
+            FinalVideo.Start();
+            
+
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            //writer.Close();
+            FinalVideo.Stop();
+            //this.Close();
+        }
+
+        private void WebCam_Leave(object sender, EventArgs e)
+        {
+            if (FinalVideo != null) FinalVideo.Stop();
+        }
+
+        
+
+       // public event EventHandler newBarCode;
+        public string barCode;
+      
+
+        private void agg_Click(object sender, EventArgs e)
+        {
+            Prodotto prod;
+            
+            string code = XZingDecode.decode(cam.Image);
+
+            string[] token = code.Split(';');
+
+            
+
+            if (code != "" && code != codiceLetto.Text)
+            {
+                prod = new Prodotto(token[0], code, token[1]);
+                barCode = code;
+                codiceLetto.Text = code;
+                Console.Beep();
+                //newBarCode(this, e);
+                string q = "INSERT INTO prodotti (Nome,dataScadenza) values('"+prod.getNome()+"','"+prod.getDataScadenza()+"');";
+                conn.ExecuteQuery(q);
+
+                conn.ChiudiConnessione();
+            }
+            
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            agg.PerformClick(); 
+                   
+        }
+
+        private void info_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Posiziona il codice a barre del tuo prodotto davanti alla Webcam, non toglierlo fino al 'beep' che ne indicherà l' avvenuta lettura", "Info", MessageBoxButtons.OK, MessageBoxIcon.Question);
+        }
+
+        private void Menu_Click(object sender, EventArgs e)
+        {
+            stop.PerformClick();
+            this.Visible = false;
+            
+        }
+    
+        
+    }
+} 
