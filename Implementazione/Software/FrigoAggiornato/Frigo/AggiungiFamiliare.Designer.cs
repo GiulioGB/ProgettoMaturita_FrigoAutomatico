c42e31b256d3e587ef1ace4fa0ff7475ed8c0604
Ballabio Giulio <giuliogb1997@gmail.com>
2016-05-27 19:26:43 +0200

    aggiunto Aggiungi familiare aggiornato

---
+++ Implementazione/Software/FrigoAggiornato/Frigo/AggiungiFamiliare.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Frigo
+{
+    partial class AggiungiFamiliare
+    {
+        /// <summary> 
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione componenti
+
+        /// <summary> 
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare 
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Menu = new System.Windows.Forms.Button();
+            this.Elimina = new System.Windows.Forms.Button();
+            this.AggiungiUtente6 = new System.Windows.Forms.Button();
+            this.AggiungiUtente5 = new System.Windows.Forms.Button();
+            this.AggiungiUtente4 = new System.Windows.Forms.Button();
+            this.AggiungiUtente3 = new System.Windows.Forms.Button();
+            this.AggiungiUtente2 = new System.Windows.Forms.Button();
+            this.AggiungiUtente1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Menu
+            // 
+            this.Menu.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Menu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.Menu.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Menu.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.Menu.FlatAppearance.BorderSize = 3;
+            this.Menu.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Menu.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Menu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Menu.Font = new System.Drawing.Font("Cambria", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Menu.Location = new System.Drawing.Point(1034, 395);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(300, 300);
+            this.Menu.TabIndex = 32;
+            this.Menu.Text = "Menu\'";
+            this.Menu.UseVisualStyleBackColor = false;
+            this.Menu.Click += new System.EventHandler(this.Menu_Click);
+            // 
+            // Elimina
+            // 
+            this.Elimina.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Elimina.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Elimina.BackgroundImage = global::Frigo.Properties.Resources.Impostazioni;
+            this.Elimina.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Elimina.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Elimina.FlatAppearance.BorderColor = System.Drawing.Color.Maroon;
+            this.Elimina.FlatAppearance.BorderSize = 3;
+            this.Elimina.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.Elimina.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Elimina.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Elimina.Font = new System.Drawing.Font("Cambria", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Elimina.Location = new System.Drawing.Point(1034, 54);
+            this.Elimina.Name = "Elimina";
+            this.Elimina.Size = new System.Drawing.Size(300, 300);
+            this.Elimina.TabIndex = 31;
+            this.Elimina.UseVisualStyleBackColor = false;
+            this.Elimina.Click += new System.EventHandler(this.Elimina_Click);
+            // 
+            // AggiungiUtente6
+            // 
+            this.AggiungiUtente6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.AggiungiUtente6.BackColor = System.Drawing.Color.White;
+            this.AggiungiUtente6.BackgroundImage = global::Frigo.Properties.Resources.pulsante_registrazione;
+            this.AggiungiUtente6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.AggiungiUtente6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AggiungiUtente6.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.AggiungiUtente6.FlatAppearance.BorderSize = 3;
+            this.AggiungiUtente6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.AggiungiUtente6.Font = new System.Drawing.Font("Cambria", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AggiungiUtente6.Location = new System.Drawing.Point(702, 395);
+            this.AggiungiUtente6.Name = "AggiungiUtente6";
+            this.AggiungiUtente6.Size = new System.Drawing.Size(300, 300);
+            this.AggiungiUtente6.TabIndex = 30;
+            this.AggiungiUtente6.UseVisualStyleBackColor = false;
+            this.AggiungiUtente6.Click += new System.EventHandler(this.AggiungiUtente6_Click);
+            // 
+            // AggiungiUtente5
+            // 
+            this.AggiungiUtente5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.AggiungiUtente5.BackColor = System.Drawing.Color.White;
+            this.AggiungiUtente5.BackgroundImage = global::Frigo.Properties.Resources.pulsante_registrazione;
+            this.AggiungiUtente5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.AggiungiUtente5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AggiungiUtente5.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.AggiungiUtente5.FlatAppearance.BorderSize = 3;
+            this.AggiungiUtente5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.AggiungiUtente5.Font = new System.Drawing.Font("Cambria", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AggiungiUtente5.Location = new System.Drawing.Point(702, 54);
+            this.AggiungiUtente5.Name = "AggiungiUtente5";
+            this.AggiungiUtente5.Size = new System.Drawing.Size(300, 300);
+            this.AggiungiUtente5.TabIndex = 29;
+            this.AggiungiUtente5.UseVisualStyleBackColor = false;
+            this.AggiungiUtente5.Click += new System.EventHandler(this.AggiungiUtente5_Click);
+            // 
+            // AggiungiUtente4
+            // 
+            this.AggiungiUtente4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.AggiungiUtente4.BackColor = System.Drawing.Color.White;
+            this.AggiungiUtente4.BackgroundImage = global::Frigo.Properties.Resources.pulsante_registrazione;
+            this.AggiungiUtente4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.AggiungiUtente4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AggiungiUtente4.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.AggiungiUtente4.FlatAppearance.BorderSize = 3;
+            this.AggiungiUtente4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.AggiungiUtente4.Font = new System.Drawing.Font("Cambria", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AggiungiUtente4.Location = new System.Drawing.Point(371, 395);
+            this.AggiungiUtente4.Name = "AggiungiUtente4";
+            this.AggiungiUtente4.Size = new System.Drawing.Size(300, 300);
+            this.AggiungiUtente4.TabIndex = 28;
+            this.AggiungiUtente4.UseVisualStyleBackColor = false;
+            this.AggiungiUtente4.Click += new System.EventHandler(this.AggiungiUtente4_Click);
+            // 
+            // AggiungiUtente3
+            // 
+            this.AggiungiUtente3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.AggiungiUtente3.BackColor = System.Drawing.Color.White;
+            this.AggiungiUtente3.BackgroundImage = global::Frigo.Properties.Resources.pulsante_registrazione;
+            this.AggiungiUtente3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.AggiungiUtente3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AggiungiUtente3.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.AggiungiUtente3.FlatAppearance.BorderSize = 3;
+            this.AggiungiUtente3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.AggiungiUtente3.Font = new System.Drawing.Font("Cambria", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AggiungiUtente3.Location = new System.Drawing.Point(371, 54);
+            this.AggiungiUtente3.Name = "AggiungiUtente3";
+            this.AggiungiUtente3.Size = new System.Drawing.Size(300, 300);
+            this.AggiungiUtente3.TabIndex = 27;
+            this.AggiungiUtente3.UseVisualStyleBackColor = false;
+            this.AggiungiUtente3.Click += new System.EventHandler(this.AggiungiUtente3_Click);
+            // 
+            // AggiungiUtente2
+            // 
+            this.AggiungiUtente2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.AggiungiUtente2.BackColor = System.Drawing.Color.White;
+            this.AggiungiUtente2.BackgroundImage = global::Frigo.Properties.Resources.pulsante_registrazione;
+            this.AggiungiUtente2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.AggiungiUtente2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AggiungiUtente2.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.AggiungiUtente2.FlatAppearance.BorderSize = 3;
+            this.AggiungiUtente2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.AggiungiUtente2.Font = new System.Drawing.Font("Cambria", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AggiungiUtente2.Location = new System.Drawing.Point(33, 395);
+            this.AggiungiUtente2.Name = "AggiungiUtente2";
+            this.AggiungiUtente2.Size = new System.Drawing.Size(300, 300);
+            this.AggiungiUtente2.TabIndex = 26;
+            this.AggiungiUtente2.UseVisualStyleBackColor = false;
+            this.AggiungiUtente2.Click += new System.EventHandler(this.AggiungiUtente2_Click);
+            // 
+            // AggiungiUtente1
+            // 
+            this.AggiungiUtente1.BackColor = System.Drawing.Color.White;
+            this.AggiungiUtente1.BackgroundImage = global::Frigo.Properties.Resources.pulsante_registrazione;
+            this.AggiungiUtente1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.AggiungiUtente1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AggiungiUtente1.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.AggiungiUtente1.FlatAppearance.BorderSize = 3;
+            this.AggiungiUtente1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.AggiungiUtente1.Font = new System.Drawing.Font("Cambria", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AggiungiUtente1.Location = new System.Drawing.Point(33, 54);
+            this.AggiungiUtente1.Name = "AggiungiUtente1";
+            this.AggiungiUtente1.Size = new System.Drawing.Size(300, 300);
+            this.AggiungiUtente1.TabIndex = 25;
+            this.AggiungiUtente1.UseVisualStyleBackColor = false;
+            this.AggiungiUtente1.Click += new System.EventHandler(this.AggiungiUtente1_Click);
+            // 
+            // AggiungiFamiliare
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.Controls.Add(this.Menu);
+            this.Controls.Add(this.Elimina);
+            this.Controls.Add(this.AggiungiUtente6);
+            this.Controls.Add(this.AggiungiUtente5);
+            this.Controls.Add(this.AggiungiUtente4);
+            this.Controls.Add(this.AggiungiUtente3);
+            this.Controls.Add(this.AggiungiUtente2);
+            this.Controls.Add(this.AggiungiUtente1);
+            this.MinimumSize = new System.Drawing.Size(1366, 749);
+            this.Name = "AggiungiFamiliare";
+            this.Size = new System.Drawing.Size(1366, 749);
+            this.Load += new System.EventHandler(this.AggiungiFamiliare_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AggiungiUtente5;
+        private System.Windows.Forms.Button AggiungiUtente4;
+        private System.Windows.Forms.Button AggiungiUtente3;
+        private System.Windows.Forms.Button AggiungiUtente2;
+        private System.Windows.Forms.Button AggiungiUtente1;
+        private System.Windows.Forms.Button AggiungiUtente6;
+        private System.Windows.Forms.Button Elimina;
+        private System.Windows.Forms.Button Menu;
+
+    }
+}
---
+++ Implementazione/Software/FrigoAggiornato/Frigo/AggiungiFamiliare.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Frigo
+{
+    public partial class AggiungiFamiliare : UserControl
+    {
+        ConnessioneDB conn = new ConnessioneDB();
+        String FrigoScelto;
+
+        public AggiungiFamiliare()
+        {
+            InitializeComponent();
+
+        }
+
+        public AggiungiFamiliare(string nomeFrigo)
+        {
+            InitializeComponent();
+            FrigoScelto = nomeFrigo;
+        }
+
+        private void AggiungiFamiliare_Load(object sender, EventArgs e)
+        {
+            //carico i familiari aggiunti in precedenza nel DB
+            conn.leggiUtenti(FrigoScelto,AggiungiUtente1, AggiungiUtente2, AggiungiUtente3, AggiungiUtente4, AggiungiUtente5, AggiungiUtente6);
+            //---
+            
+            //non faccio vedere i pulsanti vuoti
+            if(AggiungiUtente2.Text=="")
+            {
+                AggiungiUtente2.Visible = false;
+            }
+            if(AggiungiUtente3.Text == "")
+            {
+                AggiungiUtente3.Visible = false;
+            }
+            if (AggiungiUtente4.Text == "")
+            {
+                AggiungiUtente4.Visible = false;
+            }
+            if (AggiungiUtente5.Text == "")
+            {
+                AggiungiUtente5.Visible = false;
+            }
+            if (AggiungiUtente6.Text == "")
+            {
+                AggiungiUtente6.Visible = false;
+            }
+            //do la possibilità man mano di aggiungerli
+            if(AggiungiUtente1.Text != "")
+            {
+                AggiungiUtente2.Visible = true;
+            }
+            if (AggiungiUtente2.Text != "")
+            {
+                AggiungiUtente3.Visible = true;
+            }
+            if (AggiungiUtente3.Text != "")
+            {
+                AggiungiUtente4.Visible = true;
+            }
+            if (AggiungiUtente4.Text != "")
+            {
+                AggiungiUtente5.Visible = true;
+            }
+            if (AggiungiUtente5.Text != "")
+            {
+                AggiungiUtente6.Visible = true;
+            }
+            //---
+        }
+
+        private void Menu_Click(object sender, EventArgs e)
+        {
+            MenuIniziale quintaSchermata = new MenuIniziale(FrigoScelto);
+            Controls.Add(quintaSchermata);
+            quintaSchermata.BringToFront();
+            this.Refresh();
+        }
+
+        private void AggiungiUtente1_Click(object sender, EventArgs e)
+        {
+            if (AggiungiUtente1.Text == "")
+            {
+                FormUtente quartaSchermata = new FormUtente(FrigoScelto);
+                Controls.Add(quartaSchermata);
+                quartaSchermata.BringToFront();
+                this.Refresh();
+            }
+        }
+
+        private void AggiungiUtente2_Click(object sender, EventArgs e)
+        {
+            if (AggiungiUtente2.Text == "")
+            {
+                FormUtente quartaSchermata = new FormUtente(FrigoScelto);
+                Controls.Add(quartaSchermata);
+                quartaSchermata.BringToFront();
+                this.Refresh();
+            }
+        }
+
+        private void AggiungiUtente3_Click(object sender, EventArgs e)
+        {
+            if (AggiungiUtente3.Text == "")
+            {
+                FormUtente quartaSchermata = new FormUtente(FrigoScelto);
+                Controls.Add(quartaSchermata);
+                quartaSchermata.BringToFront();
+                this.Refresh();
+            }
+        }
+
+        private void AggiungiUtente4_Click(object sender, EventArgs e)
+        {
+            if (AggiungiUtente4.Text == "")
+            {
+                FormUtente quartaSchermata = new FormUtente(FrigoScelto);
+                Controls.Add(quartaSchermata);
+                quartaSchermata.BringToFront();
+                this.Refresh();
+            }
+            
+        }
+
+        private void AggiungiUtente5_Click(object sender, EventArgs e)
+        {
+            if(AggiungiUtente5.Text == "")
+            {
+                FormUtente quartaSchermata = new FormUtente(FrigoScelto);
+                Controls.Add(quartaSchermata);
+                quartaSchermata.BringToFront();
+                this.Refresh();
+            }
+            
+        }
+
+        private void AggiungiUtente6_Click(object sender, EventArgs e)
+        {
+            if (AggiungiUtente6.Text == "")
+            {
+                FormUtente quartaSchermata = new FormUtente(FrigoScelto);
+                Controls.Add(quartaSchermata);
+                quartaSchermata.BringToFront();
+                this.Refresh();
+            }
+
+        }
+
+        private void Elimina_Click(object sender, EventArgs e)
+        {
+            EliminaFamiliare el = new EliminaFamiliare(FrigoScelto);
+            Controls.Add(el);
+            el.BringToFront();
+            this.Refresh();
+        }
+    }
+}
